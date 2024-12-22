36d847cda783b272d28881bb68f3fb25cf043069
erdemaltu <e-altug@hotmail.com>
2024-12-22 20:42:53 +0300

    Courses model created

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost/smartedu-db')
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Template Engine
 app.set('view engine', 'ejs');
@@ -11,6 +23,7 @@ app.use(express.static('public'));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
