b1f269c663076c264039f1871557b86dbe5deaab
erdemaltu <e-altug@hotmail.com>
2024-12-22 21:13:54 +0300

    Coueses page created

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,12 +1,26 @@
 const Course = require('../models/Course');
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
-
   try {
+    const course = await Course.create(req.body);
     res.status(201).json({
       status: 'success',
       course,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).render('courses', {
+      page_name: 'courses',
+      courses,
     });
   } catch (error) {
     res.status(400).json({
