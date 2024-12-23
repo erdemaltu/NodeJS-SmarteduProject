7c30f18e484afae2ec600496e25fc58149ac85e5
erdemaltu <e-altug@hotmail.com>
2024-12-23 17:59:10 +0300

    Login page created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-exports.createUser = async (req, res) => {  
+exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json({
@@ -8,6 +9,32 @@ exports.createUser = async (req, res) => {
       message: 'User created successfully',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      await bcrypt.compare(password, user.password, (err, same) => {
+      if (same) {
+        //user session
+        res.status(200).send('Login successful');
+      }
+    });
+    } else {
+      res.status(404).json({
+        status: 'fail',
+        message: 'User not found',
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
