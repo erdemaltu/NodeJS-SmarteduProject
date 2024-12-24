9d46e82d145983aa84b7c242a3083f0d10ae251b
erdemaltu <e-altug@hotmail.com>
2024-12-24 15:39:35 +0300

    Course creation completed

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -4,7 +4,7 @@ const roleMiddleware = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
-router.route('/').post(roleMiddleware(['teacher','admin']),courseController.createCourse);
+router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
