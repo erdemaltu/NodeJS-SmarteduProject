cfc81037682b6f9fba5c938489b5e1441dd45cf9
erdemaltu <e-altug@hotmail.com>
2024-12-26 23:25:24 +0300

    Categories addition and deletion

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,10 +3,22 @@ const Category = require('../models/Category');
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      category,
+    req.flash("success", `${category.name} has been added successfully`);
+    res.status(201).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
     });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    
+    req.flash("success", `${category.name} has been removed successfully`);
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
